3a4eb3cc49023639ab5095f0ab298ca7b7f941a5
David Sauerwein <david.sauerwein@fau.de>
2021-02-16 01:12:41 +0100

    CardUpdater: Add overwrite_target_field parameter

--- cardupdater.py
+++ cardupdater.py
@@ -12,37 +12,49 @@ class CardUpdater:
     def add_synthesized_speech_for_query(self,
                                          source_field: str,
                                          target_field: str,
-                                         filter_query: str) -> dict:
+                                         filter_query: str,
+                                         overwrite_target_field: bool = False) -> dict:
         result = {}
+
         note_infos = self.anki_connect_client.get_note_infos(filter_query)
         for idx, note_info in enumerate(note_infos):
             print(f'\rProcessing note {idx + 1}/{len(note_infos)}', end='', flush=True)
-            changed = self.add_synthesized_speech_to_note(source_field, target_field, note_info)
+            changed = self.add_synthesized_speech_to_note(source_field,
+                                                          target_field,
+                                                          note_info,
+                                                          overwrite_target_field=overwrite_target_field)
             result[note_info['noteId']] = changed
         print()
+
         return result
 
     def add_synthesized_speech_to_note(self,
                                        source_field: str,
                                        target_field: str,
-                                       note_info: dict) -> bool:
+                                       note_info: dict,
+                                       overwrite_target_field: bool = False) -> bool:
         source_value = note_info['fields'][source_field]['value']
         target_value = note_info['fields'][target_field]['value']
         stripped_source = source_value.replace('\n', ' ').strip()
         media_filename = f'anki_speech_generator-{stripped_source}-{self.speech_synthesizer.voice.name}.wav'
         sound_string = f'[sound:{media_filename}]'
 
-        # Has this note has already been processed in a previous run of this programme?
-        if sound_string in target_value:
-            return False
-
         # Is there already a synthesized version of the source_value in the database?
         if not self.anki_connect_client.media_exists(media_filename):
             data = self.speech_synthesizer.synthesize_to_base64_string(source_value)
             self.anki_connect_client.add_media(media_filename, data)
 
-        # Update target value and tag note
-        target_value = target_value + sound_string
+        # Has this note has already been processed in a previous run?
+        if overwrite_target_field:
+            if sound_string == target_value:
+                return False
+            target_value = sound_string
+        else:
+            if sound_string in target_value:
+                return False
+            target_value = target_value + sound_string
+
+        # Update target field and tag note
         note_id = note_info['noteId']
         self.anki_connect_client.update_note_field(note_id, target_field, target_value)
         self.anki_connect_client.tag_note(note_id, 'anki-speech-generator')
